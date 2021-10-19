2036445b0f04cac54214db89cd2b965c82b0c12d
melwin123 <melwin85@gmail.com>
2021-10-19 17:57:03 +0530

    Test

--- src/main/java/com/devopsdemo/helper/GenericResourceBundle.java
+++ src/main/java/com/devopsdemo/helper/GenericResourceBundle.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 public class GenericResourceBundle {
 	public static String getProperties(String source){
+		System.out.println("Hi");
 		ResourceBundle rb = ResourceBundle.getBundle("ResourceBundle");
 		Enumeration <String> keys = rb.getKeys();
 		String value="";
